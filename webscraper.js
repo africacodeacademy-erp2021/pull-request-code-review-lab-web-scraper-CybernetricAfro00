3a0a6b5247a0fd2713f3de9570b51dc2c82fdaba
Amogelang Tshukudu <amogelang.tshukudu@botho.ac.bw>
2021-08-24 17:31:55 +0200

    additional outputs

--- webscraper.js
+++ webscraper.js
@@ -2,9 +2,7 @@ const request = require("request");
 const cheerio = require("cheerio");
 
 const images = [];
-request(
-  "https://www.istockphoto.com/illustrations/programmer",
-  (req, res, body) => {
+request("https://www.istockphoto.com/illustrations/programmer", (req, res, body) => {
     if (!console.error() && res.statusCode === 200) {
       const $ = cheerio.load(body);
       $("img", "div.Gallery-module__rowContainer___3uUNM").each(function () {
@@ -12,6 +10,17 @@ request(
         images.push(img);
       });
       console.log(images);
-    }
-  },
-);
+    
+    });
+
+// links
+
+got(link)
+.then((response) => {
+  const $ = cheerio.load(response.body);
+
+  $("a").each((i, link) => {
+    const { href } = link.attribs;
+    console.log(href);
+  });
+})
